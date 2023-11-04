0e937b918fcc185912506aad684e280c6358855a
ShreyasSridhar <ss649@buffalo.edu>
2023-11-04 16:24:38 -0400

    template, view

--- smartjournal/journal/views.py
+++ smartjournal/journal/views.py
@@ -1,8 +1,8 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import FormView
+from django.views.generic import ListView, TemplateView, FormView
 from django.urls import reverse
 from django.contrib import messages
 
@@ -34,3 +34,31 @@ class JournalAddView(LoginRequiredMixin, FormView):
     def get_success_url(self):
         messages.success(self.request, 'Added journal entry.')
         return reverse('journal_add')
+class JournalDetailView(LoginRequiredMixin, TemplateView):
+    def test_func(self):
+        """ UserPassesTestMixin Tests"""
+        if self.request.user.is_superuser:
+            return True
+
+        journal_obj = get_object_or_404(Journal, pk=self.kwargs.get('pk'))
+
+        if journal_obj.user == self.request.user:
+            return True
+
+        # if journal_obj.projectuser_set.filter(user=self.request.user, role__name='Manager', status__name='Active').exists():
+        #     return True
+
+        messages.error(self.request, 'You do not have permission to download all notes.')
+    
+    def get_context_data(self, kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        # allocation_obj = get_object_or_404(Allocation, pk=pk)
+        journal_obj = get_object_or_404(Journal, pk=pk)
+        context["jounral"] = journal_obj
+
+        return context
+    
+
+
+
