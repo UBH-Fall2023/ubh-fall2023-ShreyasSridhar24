fc3e8566f3be27070601cf310e600ffa6ec9e13e
riathetechie@gmail.com <74742605+rg663@users.noreply.github.com>
2023-11-04 16:18:18 -0400

    push

--- smartjournal/smartjournal/urls.py
+++ smartjournal/smartjournal/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('journal/', include('journal.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
