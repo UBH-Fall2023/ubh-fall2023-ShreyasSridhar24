eea5eb7f42220de2ee5569aa9e8904ffbc791f8a
ShreyasSridhar <ss649@buffalo.edu>
2023-11-04 15:49:32 -0400

    journal model added

--- smartjournal/journal/models.py
+++ smartjournal/journal/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Journal(TimeStampedModel):
     title =  models.TextField()
     user = models.ForeignKey(User)
+
     message = models.TextField()
     date = models.DateTimeField()
     is_private = models.BooleanField(default=False)
-
+    # is_therapy = models.BooleanField(default=False)
