08b859625ccf3237a0bb16c2ebafbcb0f480d733
riathetechie@gmail.com <74742605+rg663@users.noreply.github.com>
2023-11-05 11:56:39 -0500

    push home

--- smartjournal/smartjournal/urls.py
+++ smartjournal/smartjournal/urls.py
@@ -33,3 +33,4 @@ urlpatterns = [
         name='login'
     ),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
