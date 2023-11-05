868b9fe5a3e463ad2e6c2585c5c8ae508397228b
riathetechie@gmail.com <74742605+rg663@users.noreply.github.com>
2023-11-05 11:45:03 -0500

    push

--- smartjournal/journal/urls.py
+++ smartjournal/journal/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 import journal.views as views
 
 urlpatterns = [
-    path('home', TemplateView.as_view(template_name='homepage.html')),
+    path('home', TemplateView.as_view(template_name='journal/homepage.html')),
     path('', views.JournalAddView.as_view(), name='journal-add'),
     path('journal-detail/<int:pk>', views.JournalDetailView.as_view(), name='journal-detail'),
     path('journal-dashboard', views.JournalOverview.as_view(), name='journal-dashboard'),
